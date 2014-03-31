b0012e21e28dab52618b751e112c34463cf311d9
gssiyankai <syk.greg@gmail.com>
2014-03-31 22:16:00 +0200

    longest sub sequence

--- lgis/longest_sub_sequence.rb
+++ lgis/longest_sub_sequence.rb
@@ -1,30 +1,34 @@
 class LongestSubsequence
 
   def self.find_increasing_sub_sequence seq
-    find(seq, :increasing)
+    find(seq)
   end
 
   def self.find_decreasing_sub_sequence seq
-    find(seq, :decreasing)
+    find(seq.reverse).reverse
   end
 
   private
-  def self.find seq, direction
-    case direction
-      when :increasing
-        op = :<
-      when :decreasing
-        op = :>
-    end
+  def self.find seq
     seq[1..-1].reduce([[seq[0]]]) { |acc, i|
       acc.map { |sub_seq|
-        if sub_seq.last.send(op, i)
+        if sub_seq.last < i
           sub_seq << i
           [sub_seq]
         else
-          [sub_seq, sub_seq.reverse.drop_while { |v| i.send(op, v) }.reverse + [i]]
+          [sub_seq, sub_seq.reverse.drop_while { |v| v > i }.reverse + [i]]
         end
       }.flatten(1)
+       .reduce({}) { |acc, i|
+          max_sub_seq = acc[i.last]
+          if max_sub_seq.nil?
+            max_sub_seq = i
+          else
+            max_sub_seq = i unless i.size < max_sub_seq.size
+          end
+          acc[i.last] = max_sub_seq
+          acc
+       }.values
     }.max_by {|sub_seq| [sub_seq.size, -seq.find_index(sub_seq.last)] }
   end
 
