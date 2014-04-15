5d387c7259490f8a179d8ef75adf98897cc084e2
gssiyankai <syk.greg@gmail.com>
2014-04-15 21:47:19 +0200

    trees

--- tree/tree.rb
+++ tree/tree.rb
@@ -2,26 +2,20 @@ class Tree
 
 	def self.minimum_edges n, edges
 		edges.reduce([]) { |trees, edge|
-			if not contains?(trees, edge[0]) and not contains?(trees, edge[1])
-				trees << edge
-			else
-				trees = trees.map { |sub_tree|
-						if (sub_tree & edge).empty?
-							sub_tree
-						else
-							sub_tree | edge 
-						end
-					    }
-			end
-			trees
+      trees = trees.map { |tree|
+                      if (tree & edge).empty?
+                        tree
+                      else
+                        tree | edge
+                      end
+                    }
+      if trees.empty? or
+         not trees.map { |tree| (tree & edge).empty? }
+               .include?(false)
+        trees << edge
+      end
+      trees
 		}.size
-	end
-
-	private
-
-	def self.contains? trees, vertex
-		trees.map { |tree| tree.include? vertex }
-		     .include?(true)			
 	end
 
 end
