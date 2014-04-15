81e6d7ab651012fd41ecf93360b7b20a8925a1d5
gssiyankai <syk.greg@gmail.com>
2014-04-15 23:54:38 +0200

    trees

--- tree/tree.rb
+++ tree/tree.rb
@@ -1,29 +1,35 @@
 class Tree
 
-  def self.minimum_edges edges
-    edges.reduce([]) { |trees, edge|
-        trees = trees.map { |tree|
-                        if (tree & edge).empty?
-                          tree
-                        else
-                          tree | edge
-                        end
-                      }
-        trees = trees.reduce([]) { |acc, tree|
-                          if not acc.empty? and not (acc.last & tree).empty?
-                            acc[-1] = acc.last | tree
-                          else
-                            acc << tree
-                          end
-                          acc
-                       }
-        if trees.empty? or
-             not trees.map { |tree| (tree & edge).empty? }
-                      .include?(false)
-            trees << edge
-        end
-        trees
-      }.size
+  def self.minimum_edges n, edges
+    trees = edges.reduce([]) { |trees, edge|
+                trees = merge trees, edge
+                trees.reduce([]) { |acc, tree|
+                                    merge(acc, tree)
+                                 }
+            }
+    single_vertices = (1..n).reject { |i| contains?(trees, [i]) }
+    trees.size + single_vertices.size - 1
+  end
+
+  private
+
+  def self.contains? trees, tree
+    trees.map { |t| not (t & tree).empty? }
+         .include?(true)
+  end
+
+  def self.merge trees, tree
+    merge = trees.map { |t|
+                    if (t & tree).empty?
+                      t
+                    else
+                      t | tree
+                    end
+                 }
+    if not contains?(merge, tree)
+      merge << tree
+    end
+    merge
   end
 
 end
--- tree/tree_test.rb
+++ tree/tree_test.rb
@@ -5,23 +5,25 @@ class TreeTest < Test::Unit::TestCase
 
   def read_dataset f
     lines = File.readlines(f)
-    lines[1..-1].map { |line|
-                  line.split(' ').map(&:to_i)
-                }
+    n = lines.first.to_i
+    edges = lines[1..-1].map { |line|
+                          line.split(' ').map(&:to_i)
+                        }
+    [n, edges]
   end
 
   def test_simple
-    edges = read_dataset 'small_dataset.txt'
-    assert_equal 3, Tree.minimum_edges(edges)
+    n, edges = read_dataset 'small_dataset.txt'
+    assert_equal 3, Tree.minimum_edges(n, edges)
   end
 
   def test_merge
-    assert_equal 2, Tree.minimum_edges([[1,2],[3,4],[2,3],[5,6]])
+    assert_equal 1, Tree.minimum_edges(6, [[1,2],[3,4],[2,3],[5,6]])
   end
 
   def test_big_dataset
-    edges = read_dataset 'rosalind_tree.txt'
-    assert_equal 238, Tree.minimum_edges(edges)
+    n, edges = read_dataset 'rosalind_tree.txt'
+    assert_equal 85, Tree.minimum_edges(n, edges)
   end
 
 end
