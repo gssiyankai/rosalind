0842c2456b3a5ba3c32fc99490942c6ffd99b556
gssiyankai <syk.greg@gmail.com>
2014-04-18 17:09:52 +0200

    RNA Splicing

--- kmp/motif.rb
+++ kmp/motif.rb
@@ -1,9 +1,15 @@
-dna = File.readlines('small_dataset.txt')[1..-1].map(&:strip).join
+dna = File.readlines('rosalind_kmp.txt')[1..-1].map(&:strip).join
 
-failure_array = [0] + 
-		(1..dna.size-1).map { |i| 
-					max = (0..i-1).reverse_each.drop_while { |len| dna[0..len] != dna[i-len..i] }.first
-					(max||-1) + 1
+
+failure_array = (1..dna.size-1).reduce([0]) { |acc, i|
+					len = acc.last
+					if dna[len] == dna[i]
+						acc << len + 1
+					else
+						max = (0..len).reverse_each.drop_while { |len| dna[0..len] != dna[i-len..i] }.first
+						acc << 	(max||-1) + 1
+					end
+					acc
 				}
 
 puts failure_array.join(' ')
