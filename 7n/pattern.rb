5b765d37ec323682cd246632e6a3d440667bf28e
gssiyankai <syk.greg@gmail.com>
2014-04-18 23:11:25 +0200

    Multiple Pattern Matching Problem

---
+++ 7n/pattern.rb
@@ -0,0 +1,11 @@
+lines = File.readlines('rosalind_7n.txt').map(&:strip)
+
+s = lines[0]
+patterns = lines[1..-1]
+
+p (0..s.size-1).select { |i|
+  p i
+                not patterns.drop_while { |p|
+                  not s[i..-1].start_with? p
+                }.empty?
+              }
